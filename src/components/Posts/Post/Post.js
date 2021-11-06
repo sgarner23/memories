a2810ce46db6a68451c2cd7fa5a0bd714d48be05
Stephen Garner <stephenallen.garner@gmail.com>
2021-11-06 12:31:13 -0600

    sent and received post from DB

---
+++ src/components/Posts/Post/Post.js
@@ -0,0 +1,29 @@
+import React from "react";
+import useStyles from "./styles";
+import {
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+} from "@material-ui/core";
+import ThumpUpAltIcon from "@material-ui/icons/ThumbUpAlt";
+import DeleteIcon from "@material-ui/icons/Delete";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+
+const Post = ({ post }) => {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.card}>
+      <CardMedia
+        className={classes.media}
+        image={post.selectedFile}
+        title={post.title}
+      />
+    </Card>
+  );
+};
+
+export default Post;
